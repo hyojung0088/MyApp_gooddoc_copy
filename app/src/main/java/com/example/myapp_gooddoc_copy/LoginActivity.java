ced86f796e48ac84d612de3174beab8b78547f5b
User <ha0801@nate.com>
2019-04-14 15:21:04 +0900

    로그인 버튼 이벤트 추가

--- app/src/main/java/com/example/myapp_gooddoc_copy/LoginActivity.java
+++ app/src/main/java/com/example/myapp_gooddoc_copy/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapp_gooddoc_copy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,7 +31,8 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (userId.equals("tjeit") && userPw.equals("1234"))
                 {
-
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
                 }
                 else
                 {
